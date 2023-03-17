c68756287f725148237c323268ba5ed95a56e2e1
Parisa Esmaeilian <parisaes@Parisas-MacBook-Air.local>
2023-03-16 18:46:29 -0700

    first commit: transform a dataset of edges to adjacency lists (to-adjacency-list.py), PCSS MapReduce (pcss.py)

---
+++ pcss.py
@@ -0,0 +1,53 @@
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+import numpy as np
+import csv
+import sys
+
+threshold = 0.5
+remaining_edges = []
+
+class PCSS(MRJob):
+    
+    def steps(self):
+        return[
+            MRStep(mapper = self.PCSSMapper,
+            reducer = self.PCSSReducer)
+        ]
+    
+    #Mapper function 
+    def PCSSMapper(self, _, file):
+        reader = csv.reader([file])
+        for row in reader:
+            nodeID = int(row[0])
+            neighbours = [int(r) for r in row[1:]]
+            for u in neighbours:
+                yield (min(nodeID, u), max(u, nodeID)), neighbours
+            
+    #Reducer function
+    def PCSSReducer(self, key, values):
+        edgeLists = list(values)
+        structural_similarity = StructuralSimilarity(edgeLists[0], edgeLists[1])
+        if structural_similarity >= threshold:
+            remaining_edges.append(key)
+            yield key, structural_similarity  
+
+
+def Intersect(list1, list2):
+    return [value for value in list1 if value in list2]
+
+def StructuralSimilarity(edgeList1, edgeList2): # assuming no self loops
+    numerator = len(Intersect(edgeList1, edgeList2)) + 1 # u and v are connected: adding u and v to u.neighbours and v.neighbours
+    len1 = len(edgeList1) + 1 # adding u to u.neighbours
+    len2 = len(edgeList2) + 1 # adding v to v.neighbours
+    denominator = np.sqrt(len1 * len2)
+    return numerator / denominator
+
+if __name__ == "__main__":
+    PCSS.run()
+    datasetName = sys.argv[1].split("_")[0]
+    fileName = datasetName + "_filtered" + ".csv"
+    with open(fileName, mode='w') as file:
+        writer = csv.writer(file)
+        for edge in remaining_edges:
+            writer.writerow(edge)
---
+++ to-adjacency-list.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import sys
+
+def listToString(list, delimeter=","):
+    strList = [str(i) for i in list]
+    return delimeter.join(strList)
+
+def ExtractAdjacencyLists(fileName):
+    df = pd.read_csv(fileName, delimiter=",", header=None)
+    numberOfNodes = max(df[0].max(), df[1].max()) + 1 #maxID + 1
+
+    adjacencyLists = [[] for i in range(numberOfNodes)] 
+    for _, row in df.iterrows():
+        node1 = int(row[0]) #TO-DO: specify data type when building the dataFrame
+        node2 = int(row[1])
+        adjacencyLists[node1].append(node2)
+        adjacencyLists[node2].append(node1)
+
+    adjacencyListsTxt = ""
+    for ID, list in enumerate(adjacencyLists):
+        if (len(list) > 0):
+            adjacencyListsTxt += str(ID) + ","
+            adjacencyListsTxt += listToString(list)
+            if (ID < len(adjacencyLists) - 1):
+                adjacencyListsTxt += "\n"
+    return adjacencyListsTxt
+
+def WriteToFile(string, fileName):
+    f = open(fileName, "w")
+    f.write(string)
+    f.close()
+
+if __name__ == "__main__":
+    output = ExtractAdjacencyLists(sys.argv[1])
+    datasetName = sys.argv[1].split(".")[0]
+    fileName = datasetName + "_adjacency-lists.csv"
+    WriteToFile(output, fileName)
+
