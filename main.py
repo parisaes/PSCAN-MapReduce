1a0a60cdb9ce01de498a85881b47a888cc13bb52
Parisa Esmaeilian <parisaes@Parisas-MacBook-Air.local>
2023-03-30 20:20:46 -0700

    add numCores

--- main.py
+++ main.py
@@ -8,15 +8,15 @@ def toAdjacencyLists(file):
     if result.returncode != 0:
         print(result.stderr)
 
-def runPCSS(inputFileName, outputFileName):
-    command = ['python3', 'pcss.py', inputFileName]
+def runPCSS(inputFileName, outputFileName, numCores):
+    command = ['python3', 'pcss.py', inputFileName, f"--num-cores={numCores}"]
     with open(outputFileName, 'w') as f:
         result = subprocess.run(command, text=True, stdout=f)
         if result.returncode != 0:
             print(result.stderr)
 
-def runLPCC(inputFileName, outputFileName):
-    command = ['python3', 'lpcc.py', inputFileName]
+def runLPCC(inputFileName, outputFileName, numCores):
+    command = ['python3', 'lpcc.py', inputFileName, f"--num-cores={numCores}"]
     with open(outputFileName, 'w') as f:
         result = subprocess.run(command, text=True, stdout=f)
         if result.returncode != 0:
@@ -26,8 +26,12 @@ if __name__ == "__main__":
     inputFilePath = sys.argv[1].split(".")[0] # without format
     toAdjacencyLists(sys.argv[1])
 
+    numCores = 0; # number of cores to be used, value of 0 lets the programm decide
+    if (len(sys.argv) > 2):
+        numCores = sys.argv[2]
+
     SIFileName = inputFilePath + "_si.txt"
-    runPCSS(inputFilePath + "_adjacency-lists.csv", SIFileName) # first MapReduce
+    runPCSS(inputFilePath + "_adjacency-lists.csv", SIFileName, numCores) # first MapReduce
 
     tmpFileName = inputFilePath + "_tmp.txt"
     while(True):
@@ -40,7 +44,7 @@ if __name__ == "__main__":
                     bRun = True
                     break
         if (bRun):
-            runLPCC(SIFileName, tmpFileName) # second MapReduce
+            runLPCC(SIFileName, tmpFileName, numCores) # second MapReduce
             os.remove(SIFileName)
             os.rename(tmpFileName, SIFileName)
         else:
